52bac84d50f92617ca7e3f1657dd66dba2ba7f35
Siraphop <siraphop2707@gmail.com>
2026-03-11 14:48:33 +0700

    do assignment6 following slide

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,7 +1,13 @@
+'use client'
 import Image from 'next/image'
+import { useState } from "react"
 import InteractiveCard from './InteractiveCard'
+import { Rating } from "@mui/material"
 
-export default function ProductCard({venueName, imgSrc} : {venueName:string, imgSrc:string}) {
+export default function ProductCard({venueName, imgSrc, rating, onRatingChange} 
+    : {venueName:string, imgSrc:string, rating:number | undefined, onRatingChange:(value:number)=>void}) {
+
+    const [value, setValue] = useState<number | null>(0)
 
     return (
         <InteractiveCard contentName={venueName}>
@@ -12,7 +18,15 @@ export default function ProductCard({venueName, imgSrc} : {venueName:string, img
                     className='object-cover rounded-t-lg'
                 />
             </div>
-            <div className='w-full h-[30%]'>{venueName}</div>
+
+            <div className='w-full h-[30%]'>
+                {venueName}
+                <Rating value={rating} onChange={(event, newValue) => { if(newValue!=null){ onRatingChange(newValue)}}}
+                    id={venueName + " Rating"}
+                    name={venueName + " Rating"}
+                    data-testid={venueName + " Rating"}
+                />
+                </div>
         </InteractiveCard>
     )
 }
--- src/components/CardPanel.tsx
+++ src/components/CardPanel.tsx
@@ -1,1 +1,62 @@
-export default function
+'use client'
+import { useReducer } from "react";
+import Card from "@/components/Card";
+import { Rating } from "@mui/material"
+
+function ratingReducer(state: Map<string, number>, action: { type: string, venue: string, rating?: number }) {
+
+    const newState = new Map(state)
+
+    switch(action.type) {
+        case "set":
+        if(action.rating !== undefined) {
+            newState.set(action.venue, action.rating);
+        }
+        return newState;
+        case "remove":
+            newState.delete(action.venue);
+            return newState;
+        default:
+            return state;
+    }
+}
+
+
+export default function CardPanel() {
+    const initialState = new Map<string, number>([
+        ["The Bloom Pavilion", 0],
+        ["Spark Space", 0],
+        ["The Grand Table", 0]
+    ]);
+
+    const [ratings, dispatch] = useReducer(ratingReducer, initialState);
+
+    return (
+        <div style={{margin:"20px", display:"flex", flexDirection:"row", 
+        flexWrap:"wrap", justifyContent:"space-around", alignContent:"space-around"}}>
+
+        <Card venueName='The Bloom Pavilion' imgSrc="/img/bloom.jpg" 
+        rating={ratings.get("The Bloom Pavilion")}
+        onRatingChange={(value:number) => dispatch({type:"set", venue:"The Bloom Pavilion", rating:value})}
+        />
+
+        <Card venueName='Spark Space' imgSrc="/img/sparkspace.jpg" 
+        rating={ratings.get("Spark Space")}
+        onRatingChange={(value:number) => dispatch({type:"set", venue:"Spark Space", rating:value})}
+        />
+
+        <Card venueName='The Grand Table' imgSrc="/img/grandtable.jpg"
+        rating={ratings.get("The Grand Table")}
+        onRatingChange={(value:number) => dispatch({type:"set", venue:"The Grand Table", rating:value})}
+        />
+
+        <div style={{width:"100%", marginTop:"20px"}}>
+        {Array.from(ratings.entries()).map(([venue, rating]) => (<div key={venue} data-testid={venue}
+        onClick={() => dispatch({type:"remove", venue:venue})} style={{cursor:"pointer"}}
+        >{venue} Rating : {rating}</div>))}
+        </div>
+
+</div>
+
+    );
+}
